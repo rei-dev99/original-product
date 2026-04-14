50b10a706a7dd2c5c72aef020ec416d3b4c60729
rei <idareiya123@gmail.com>
2026-04-15 06:33:05 +0900

    add: header footer

---
+++ front/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 py-4 text-white">
+      <p className="text-center text-sm">&copy; 2026 理解負債チェッカー</p>
+    </footer>
+  )
+}
