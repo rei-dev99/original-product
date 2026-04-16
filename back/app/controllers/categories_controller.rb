4287b220da0a07b80a17b39d22ae4006f1e74470
rei <idareiya123@gmail.com>
2026-04-16 21:26:32 +0900

    fix: json data

--- back/app/controllers/categories_controller.rb
+++ back/app/controllers/categories_controller.rb
@@ -1,10 +1,19 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all.includes(:questions)
+    @categories = Category.all.includes(questions: :choices)
     render status: :ok,
     json: @categories.as_json(
-      include: { questions: { only: [ :id, :content ] } },
-      only: [ :id, :name ]
+      only: [ :id, :name ],
+      include: {
+        questions: {
+          only: [ :id, :content ],
+          include: {
+            choices: {
+              only: [ :id, :content, :score ]
+            }
+          }
+        }
+      },
     )
   end
 end
