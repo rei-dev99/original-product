e7a8db57165f2c0c1a5ff1d9bd8adbc532740614
rei <idareiya123@gmail.com>
2026-04-23 21:24:24 +0900

    add: if error

--- front/app/lib/result.ts
+++ front/app/lib/result.ts
@@ -2,5 +2,9 @@ export default async function fetchResult(id: string) {
   const res = await fetch(`http://localhost:3001/results/${id}`, {
     cache: "no-store"
   })
+
+  if (!res.ok) {
+    throw new Error("データが存在しません。")
+  }
   return res.json()
 }
