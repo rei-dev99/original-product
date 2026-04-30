c7f2c87dcf4244bf25c304d3b31dbf1b0de73463
rei <idareiya123@gmail.com>
2026-04-30 13:07:18 +0900

    feat: auth action

---
+++ front/actions/handleLogout.ts
@@ -0,0 +1,9 @@
+"use server";
+import { cookies } from "next/headers";
+import { signOut } from "@/auth";
+
+export async function handleLogout() {
+    const cookieStore = await cookies();
+    cookieStore.delete("current_user");
+    await signOut({ redirectTo: "/login" });
+}
