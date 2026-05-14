55d02e848019b9776dc7d1446d63a3ec2ab77464
rei <idareiya123@gmail.com>
2026-05-15 07:04:52 +0900

    feat: singup action

---
+++ front/actions/handleSignup.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { createBackendJwt } from "@/app/lib/createBackendJwt";
+
+export default async function handleSignup(formData: FormData) {
+	const email = formData.get("email");
+	const password = formData.get("password");
+
+    if (!email || !password) { return; }
+
+	const signupResponse = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/signup_email`, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ email, password }),
+	});
+
+    if (!signupResponse.ok) { return; }
+
+    await signIn("credentials", {
+        redirect: false,
+        loginType: "email",
+        email,
+        password,
+    });
+
+    const backendJwt = await createBackendJwt({
+		email: String(email),
+		provider: "email",
+		uid: String(email),
+	});
+
+    const loginResponse = await fetch(
+		`${process.env.NEXT_PUBLIC_API_BASE_URL}/login`,
+		{
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${backendJwt}`,
+			},
+			cache: "no-store",
+		},
+	);
+
+    if (!loginResponse.ok) {
+		return;
+	}
+
+    const data = await loginResponse.json();
+
+    const cookieStore = await cookies();
+
+    cookieStore.set(
+		"current_user",
+		encodeURIComponent(JSON.stringify(data.user)),
+		{
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			path: "/",
+		},
+	);
+
+	redirect("/mypage");
+
+}
