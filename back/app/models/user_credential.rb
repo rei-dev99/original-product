67c29d93fc441ec247d1aa64dd401bee99df0314
rei <idareiya123@gmail.com>
2026-05-17 08:54:04 +0900

    add: uniqueness

--- back/app/models/user_credential.rb
+++ back/app/models/user_credential.rb
@@ -6,6 +6,6 @@ class UserCredential < ApplicationRecord
   validates :provider, presence: true
   validates :uid, presence: true
 
-  validates :email, presence: true, if: -> { provider == "email" }
+  validates :email, presence: true, uniqueness: true, if: -> { provider == "email" }
   validates :password, presence: true, length: { minimum: 8 }, if: -> { provider == "email" }
 end
