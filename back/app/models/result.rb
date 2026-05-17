5e5ad39302540463b9df2d7fd7275cf0b5bbc29f
rei <idareiya123@gmail.com>
2026-05-17 13:12:28 +0900

    fix: numericality in

--- back/app/models/result.rb
+++ back/app/models/result.rb
@@ -3,9 +3,9 @@ class Result < ApplicationRecord
 
   validates :advice, presence: true
 
-  validates :ai_score,  presence: true, numericality: { in: 0..100 }
-  validates :algorithm_score,  presence: true, numericality: { in: 0..100 }
-  validates :db_score,  presence: true, numericality: { in: 0..100 }
+  validates :ai_score,  presence: true, numericality: { in: 0..15 }
+  validates :algorithm_score,  presence: true, numericality: { in: 0..15 }
+  validates :db_score,  presence: true, numericality: { in: 0..15 }
+  validates :web_score,  presence: true, numericality: { in: 0..15 }
   validates :dependency_score,  presence: true, numericality: { in: 0..100 }
-  validates :web_score,  presence: true, numericality: { in: 0..100 }
 end
