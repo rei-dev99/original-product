0d743c742a4285336eda52045f39b1fda11de42f
rei <idareiya123@gmail.com>
2026-05-17 11:01:21 +0900

    feat: model spec question

---
+++ back/spec/models/question_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  describe 'validations' do
+    describe 'presence validation' do
+      context 'when attributes are valid' do
+        let(:question) { build(:question) }
+
+        it 'is valid' do
+          expect(question).to be_valid
+        end
+      end
+
+      context 'when content is blank' do
+        let(:question) { build(:question, content: nil) }
+
+        it 'is invalid' do
+          expect(question).to be_invalid
+        end
+      end
+    end
+
+    describe 'uniqueness validation' do
+      context 'when content already exists' do
+        let!(:question_before) { create(:question, content: "エラーが出た時、まず何をしますか？") }
+        let(:question) { build(:question, content: "エラーが出た時、まず何をしますか？") }
+
+        it 'is invalid' do
+          expect(question).to be_invalid
+        end
+      end
+    end
+  end
+end
