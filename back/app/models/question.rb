f735a15160d29eea8203a02ca872aad37ec977cd
rei <idareiya123@gmail.com>
2026-05-17 11:00:43 +0900

    add: uniqueness

--- back/app/models/question.rb
+++ back/app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
   belongs_to :category
   has_many :choices, dependent: :destroy
 
-  validates :content,  presence: true
+  validates :content, presence: true, uniqueness: true
 end
